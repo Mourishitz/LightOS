a8a2eb44cc101171117553ec3b30aa6f05d7a56a
Morishita <gabrielmorishita@hotmail.com>
2023-07-04 13:50:27 -0300

    ✨ feat: loading our 32 bit kernel into memory

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -33,7 +33,6 @@ step2:
 
     jmp CODE_SEG:load32
 
-
 ; GDT
 gdt_start:
 gdt_null:
@@ -63,24 +62,69 @@ gdt_end:
 gdt_descriptor:
     dw gdt_end - gdt_start-1
     dd gdt_start
+ [BITS 32]
+ load32:
+    mov eax, 1          ; starting sector where we load from
+    mov ecx, 100        ; total number sectors we will load
+    mov edi, 0x0100000  ; address we are loading into (1Mb)
+    call ata_lba_read
+    jmp CODE_SEG:0x0100000
 
-[BITS 32]
-load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
+ata_lba_read:
+    mov ebx, eax, ; Backup the LBA
+    ; Send the highest 8 bits of the lba to hard disk controller
+    shr eax, 24   ; (38 - 28 = 8bits)
+    or eax, 0xE0  ; Select the master drive
+    mov dx, 0x1F6 ; Port that expects us to wrtie the 8bits to
+    out dx, al
+    ; Finished sending 8bits of the lba
 
-    ; Enable A20 Line
-    in al, 0x92
-    or al, 2
-    out 0x92, al
+    ; Send total sectors to read
+    mov eax, ecx
+    mov dx, 0x1F2
+    out dx, al
 
-    jmp $
+    ; Send more bits of the LBA
+    mov eax, ebx ; Restoring backup of LBA
+    mov dx, 0x1F3
+    out dx, al
+
+    ; Send more bits of the LBA
+    mov dx, 0x1F4
+    mov eax, ebx ; Restoring backup of LBA
+    shr eax, 8 
+    out dx, al
+
+    ; Send upper 16 bits of LBA
+
+    mov dx, 0x1F5
+    mov eax, ebx ; Restoring backup of LBA
+    shr eax, 16
+    out dx, al
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+; Read all sector into memory
+.next_sector:
+    push ecx
+
+; Check if we need to read
+.try_again:
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again
+
+; We need to read 256 words at a time
+    mov ecx, 256
+    mov dx, 0x1F0
+    rep insw ; Reads a word in I/O port specified in dx into memory location specified in edi
+    pop ecx  ; Basically, read 256 times the I/O ant store on the 1Mb (edi=0x0100000)
+    loop .next_sector
+
+    ret
 
 times 510-($ - $$) db 0
 dw 0xAA55 
